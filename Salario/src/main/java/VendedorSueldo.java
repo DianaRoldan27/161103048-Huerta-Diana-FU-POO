398dab32942556681949c3b5323051cf89c54902
diana pricila huerta roldan <diana.huerta1148@uppuebla.edu.mx>
2020-05-22 00:40:51 -0500

    Salario

---
+++ Salario/src/main/java/VendedorSueldo.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+//Un vendedor recibe un sueldo base 3000 mas el 10% de su venta total.
+//Un programa que calcule el sueldo de un vendedor dando su venta total
+
+public class VendedorSueldo {
+    public static void main (String args[]){
+       float sueldoBase, vuno, vdos ,ventas, comision, total;
+        
+        System.out.print("Ingresa tu sueldo base: ");
+        Scanner lee = new Scanner (System.in);
+        sueldoBase = lee.nextFloat();
+        System.out.print("Ingresa tu 1er venta: ");
+        vuno= lee.nextFloat();
+        System.out.print("Ingresa tu 2da venta: ");
+        vdos= lee.nextFloat();
+        
+        ventas= vuno+vdos;
+        comision=(ventas*10)/100;
+        total=sueldoBase+comision;
+        
+        System.out.println("\nObtendrás por concepto de comisiones: $"+comision);
+        System.out.println("Obtendrás en total: $"+total);    
+            }
+}
