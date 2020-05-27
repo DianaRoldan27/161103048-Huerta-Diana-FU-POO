ea143b06c363f8744cc6eb94a2a3fcc76a03ffb4
diana pricila huerta roldan <diana.huerta1148@uppuebla.edu.mx>
2020-05-26 21:47:58 -0500

    Pago

--- Pago/src/main/java/Pago.java
+++ Pago/src/main/java/Pago.java
@@ -1,16 +1,24 @@
 
 import java.util.Scanner;
 
-
 public class Pago {
     public static void main (String args[]){
-    double descuento, montototal,venta;
-   Scanner entrada = new Scanner (System.in);
-   
-   System.out.println("Intriduzca la ");
-   
-    
-    
+     double N,monto;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Ingrese monto de compra");
+      N=sc.nextDouble();
+      if(N>100){
+          monto= N-(N*0.10);
+          System.out.println(monto);
+      }else{
+          if(N <= 100 && N > 50){
+              monto = N-(N*0.5);
+              System.out.println(monto);
+          }else{
+              System.out.println(N);
+          }
+      }
+    }   
 } 
     
 
