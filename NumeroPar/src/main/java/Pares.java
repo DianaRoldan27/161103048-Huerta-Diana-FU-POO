6032f91649e379e2aedf3f6b0b768a82ee083bdf
diana pricila huerta roldan <diana.huerta1148@uppuebla.edu.mx>
2020-05-26 23:29:49 -0500

    Numero par

--- NumeroPar/src/main/java/Pares.java
+++ NumeroPar/src/main/java/Pares.java
@@ -1,8 +1,16 @@
 
-
+import java.util.Scanner;
 
 public class Pares {
     public static void main(String[] args) {
-       
+         Scanner scan = new Scanner(System.in);
+        int num1;
+        System.out.println("Dame un numero: ");
+        num1 = scan.nextInt();
+        
+        System.out.println(" \n");
+        for (int i = 2; i <=num1; i+=2) {
+            System.out.println(i);
+        }
     }
 }
